c5b837777cc860802e4331cc51ce2759901b9213
ENRICO\Enrico <enrico.caruso@email.it>
2017-03-11 18:14:05 +0100

    Project initiation

---
+++ BaseMVC/BaseMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BaseMVC.Startup))]
+namespace BaseMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
